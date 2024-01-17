29dc145fc0f2a531efecc3f9ce48b05667965879
jonweng1 <jonathanwengster@gmail.com>
2024-01-17 16:42:54 -0500

    Update sql_injection_search.py

--- vulns/sql_injection/sql_injection_search.py
+++ vulns/sql_injection/sql_injection_search.py
@@ -4,8 +4,6 @@ from flask import render_template
 def sql_injection_search_page(request, app):
     search = request.args.get('q')
 
-    #sql = f"SELECT * FROM products WHERE name LIKE '%{search}%'"
-
     db_result = app.db_helper.execute_read(sql)
 
     products = list(
